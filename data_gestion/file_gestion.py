1db843286a80a3ab4cdb7bdf4308edc5278d331f
Thibault Gigant <thibault.gigant@gmail.com>
2016-03-07 23:20:23 +0100

    Correction of file_gestion to handle French approval ballots. Add directory support. Add possibility to exclude candidates from structure. Correction of find_axis_from_file

--- data_gestion/file_gestion.py
+++ data_gestion/file_gestion.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env sage -python
 # -*- coding: utf-8 -*-
 import sys
+from os import listdir
+from os.path import join
 from sage.all import Set
 
 
@@ -53,7 +55,7 @@ def read_file(filename):
             # temp is the preference for a set of people :
             # the number of people having this preference, a strict order of preferences
             # If indifferent between candidates, a Set (from sagemath) of them is added to the list of preferences
-            temp = (count, [])
+            temp = [count, []]
             i = 0
             while i < len(pref):
                 p = pref[i]
@@ -68,7 +70,9 @@ def read_file(filename):
                         indiff.append(int(p))
                         i += 1
                         p = pref[i]
-                    indiff.append(int(p[:-1]))
+                    p = p.strip('{}')
+                    if p:
+                        indiff.append(int(p))
                     temp[1].append(Set(indiff))
                 i += 1
 
@@ -86,6 +90,9 @@ def read_file(filename):
     # Closing file
     fp.close()
 
+    # Just making sure the preferences are sorted by decreasing order of number of voters
+    prefs.sort(key=lambda x: x[0], reverse=True)
+
     return {"nb_candidates": nb_candidates,
             "candidates": candidates,
             "nb_voters": nb_voters,
@@ -94,11 +101,87 @@ def read_file(filename):
             "preferences": prefs}
 
 
+def read_directory(dirname):
+    """
+    Reads the files in the directory and returns concatenation of the data contents
+    :param dirname: absolute or relative path to the directory
+    :type dirname: str
+    :raise ValueError: if wrong directory, empty directory, or wrong file format
+    :return: map with all data from the files
+    """
+    try:
+        files = [join(dirname, i) for i in listdir(dirname) if i[-4:] == ".toc"]
+    except:
+        raise ValueError("No such directory")
+    if not files:
+        raise ValueError("No .toc files detected")
+
+    structure = read_file(files[0])
+
+    unique_orders = [pref[1] for pref in structure["preferences"]]
+
+    for f in files[1:]:
+        temp_struct = read_file(f)
+        structure["nb_voters"] += temp_struct["nb_voters"]
+        structure["sum_vote_count"] += temp_struct["sum_vote_count"]
+        for pref in temp_struct["preferences"]:
+            if pref[1] in unique_orders:
+                i = unique_orders.index(pref[1])
+                structure["preferences"][i][0] += pref[0]
+            else:
+                structure["preferences"].append(pref)
+                structure["nb_unique_orders"] += 1
+                unique_orders.append(pref[1])
+
+    structure["preferences"].sort(key=lambda x: x[0], reverse=True)
+    return structure
+
+
+def remove_unwanted_candidates(structure, unwanted_candidates):
+    """
+    Removes unwanted candidates to the structure, including in the preferences
+    :param structure: structure we want to remove the candidates from
+    :param unwanted_candidates: list of IDs of unwanted candidates
+    :type unwanted_candidates: list
+    :return: the new structure without the unwanted candidates
+    """
+    new_structure = {"nb_candidates": structure["nb_candidates"] - len(unwanted_candidates), "candidates": {},
+                     "nb_voters": structure["nb_voters"], "sum_vote_count": structure["sum_vote_count"],
+                     "preferences": []}
+
+    # Modifying the candidates set and creating a conversion table to make the preferences transformation easier
+    i = 1
+    conversion_table = {}
+    for candidate in structure["candidates"].keys():
+        if candidate not in unwanted_candidates:
+            new_structure["candidates"][i] = structure["candidates"][candidate]
+            conversion_table[candidate] = i
+            i += 1
+
+    # Adding preferences after deleting unwanted_candidates
+    unique_orders = []
+    for nb_votes, pref in structure["preferences"]:
+        # cleaning the list of preferences from its unwanted candidates
+        temp = [conversion_table[i] for i in pref if i not in unwanted_candidates and isinstance(i, int)]
+        if not isinstance(pref[-1], int):
+            temp.append(Set([conversion_table[i] for i in pref[-1] if i not in unwanted_candidates]))
+        # adding of the list of preferences to the structure
+        if temp in unique_orders:
+            new_structure["preferences"][unique_orders.index(temp)][0] += nb_votes
+        else:
+            new_structure["preferences"].append([nb_votes, temp])
+            unique_orders.append(temp)
+
+    new_structure["nb_unique_orders"] = len(unique_orders)
+
+    return new_structure
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit("This program takes one and only one argument")
-    file = sys.argv[1]
-    structure = read_file(file)
+    f = sys.argv[1]
+    structure = read_file(f)
     for c in structure["candidates"].items():
         print(c)
     for j in structure["preferences"]:
--- data_gestion/find_axis_from_file.py
+++ data_gestion/find_axis_from_file.py
@@ -1,36 +1,133 @@
 from sage.all import Set
-from file_gestion import read_file
+from file_gestion import *
 from similarity_matrix import *
 from time import time
 from os import listdir
 from os.path import join
+from getopt import getopt
 import sys
 
 
-def find_axis_from_file(input_file, dissimilarity_function=dissimilarity_over_over, weighted=False):
-    structure = read_file(input_file)
+def find_axis_from_file(structure, dissimilarity_function=dissimilarity_over_over, weighted=False):
+    """
+    Finds the axes coherent with the data in the structure
+    :param structure: data extracted from an election file
+    :param dissimilarity_function: function to use to calculate dissimilarity between 2 candidates
+    :param weighted: if True, matrices scores are calculated with the weighted gradient
+    :return: calculation time and optimal permutations for this structure
+    """
     candidates_set = Set(structure["candidates"].keys())
 
     t = time()
     similarity_matrix = create_similarity_matrix(structure, dissimilarity_function)
-    optimal_permutations = find_permutation_dynamic_programming(similarity_matrix, candidates_set, {}, False)
+    optimal_permutations = find_permutation_dynamic_programming(similarity_matrix, candidates_set, {}, weighted)
 
     return time()-t, optimal_permutations[candidates_set]
 
 
-def write_results_on_file(input_directory, output_file, dissimilarity_function=dissimilarity_over_over, weighted=False):
+def write_results_on_file(input_directory, output_file, dissimilarity_function=dissimilarity_over_over,
+                          weighted=False, unwanted_candidates=[]):
+    """
+    For each file in the directory, calculates the optimal axes coherent with the data and writes it on the output file
+    :param input_directory: directory where the .toc files are located
+    :param output_file: output file
+    :param dissimilarity_function: function to use to calculate dissimilarity between 2 candidates
+    :param weighted: if True, matrices scores are calculated with the weighted gradient
+    :param unwanted_candidates: list of candidates to exclude from the search
+    """
     files = [join(input_directory, i) for i in listdir(input_directory) if i[-3:] == "toc"]
     fp = open(output_file, "w")
 
     for f in files:
+        structure = read_file(f)
+        if unwanted_candidates:
+            structure = remove_unwanted_candidates(structure, unwanted_candidates)
         print(f)
         fp.write(str(f) + "\n")
-        t, optimal_permutations = find_axis_from_file(f, dissimilarity_function, weighted)
+        t, optimal_permutations = find_axis_from_file(structure, dissimilarity_function, weighted)
         print(t)
         fp.write("calculation time: " + str(t) + " seconds\n")
-        fp.write("axes: " + str(optimal_permutations) + "\n\n")
+        fp.write("axes: " + str(optimal_permutations) + "\n")
+        for axis in optimal_permutations[1]:
+            fp.write(str([structure["candidates"][i] for i in axis]) + "\n")
+        fp.write("\n")
     fp.close()
 
 
+def write_directory_results_on_file(input_directory, output_file, dissimilarity_function=dissimilarity_over_over,
+                                    weighted=False, unwanted_candidates=[]):
+    """
+    Creates a structure combining the data from all election files in the directory,
+    then calculates the optimal axes coherent with the data and writes it on the output file
+    :param input_directory: directory where the .toc files are located
+    :param output_file: output file
+    :param dissimilarity_function: function to use to calculate dissimilarity between 2 candidates
+    :param weighted: if True, matrices scores are calculated with the weighted gradient
+    :param unwanted_candidates: list of candidates to exclude from the search
+    """
+    fp = open(output_file, "w")
+
+    structure = read_directory(input_directory)
+    if unwanted_candidates:
+        structure = remove_unwanted_candidates(structure, unwanted_candidates)
+
+    fp.write(str(input_directory) + "\n")
+    t, optimal_permutations = find_axis_from_file(structure, dissimilarity_function, weighted)
+    print(t)
+    fp.write("calculation time: " + str(t) + " seconds\n")
+    fp.write("axes: " + str(optimal_permutations) + "\n")
+    for axis in optimal_permutations[1]:
+        fp.write(str([structure["candidates"][i] for i in axis]) + "\n")
+
+    fp.close()
+
+
+def launch():
+    """
+    Function used to launch the program the way we want it, using command line options
+    :return:
+    """
+    dissimilarity_function = dissimilarity_over_over
+    weighted = False
+    input_directory = ""
+    output_file = ""
+    fusion = False
+    unwanted_candidates = []
+
+    opts, args = getopt(sys.argv[1:], "d:o:w", ["func=", "fusion", "not="])
+
+    for opt, value in opts:
+        if opt == "-d":
+            input_directory = value
+        if opt == "-o":
+            output_file = value
+        if opt == "-w":
+            weighted = True
+        if opt == "--func":
+            if value == "0":
+                dissimilarity_function = dissimilarity_over_over
+            elif value == "1":
+                dissimilarity_function = dissimilarity_and_n
+            elif value == "2":
+                dissimilarity_function = dissimilarity_and_or
+        if opt == "--fusion":
+            fusion = True
+        if opt == "--not":
+            unwanted_candidates = list(map(int, value.split()))
+
+    if (not input_directory or not output_file) and len(args) != 2:
+        raise IOError("Not enough arguments")
+
+    if len(args) == 2:
+        input_directory = args[0]
+        output_file = args[1]
+
+    if fusion:
+        write_directory_results_on_file(input_directory, output_file, dissimilarity_function,
+                                        weighted, unwanted_candidates)
+    else:
+        write_results_on_file(input_directory, output_file, dissimilarity_function, weighted, unwanted_candidates)
+
+
 if __name__ == '__main__':
-    write_results_on_file(sys.argv[1], sys.argv[2])
+    launch()
