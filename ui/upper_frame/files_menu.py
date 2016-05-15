bcd5b568d3511ed8aad344b03f123315a9e09bf1
Thibault Gigant <thibault.gigant@gmail.com>
2016-05-15 21:28:21 +0200

    Add french files to UI

--- ui/upper_frame/files_menu.py
+++ ui/upper_frame/files_menu.py
@@ -7,6 +7,7 @@ class FilesMenu(LabelFrame):
     """
     Adds all widgets to select files to test
     """
+
     def __init__(self, master):
         LabelFrame.__init__(self, master, text="Choose files")
         self.parent = master
@@ -17,15 +18,20 @@ class FilesMenu(LabelFrame):
     def add_files(self):
         label = Label(self, text="Choose files:", font=("", 16))
         label.grid()
+        # for f in range(len(listFiles)):
+        #     checkbtn = Checkbutton(self, text=listFiles[f], command=lambda name=f: self.add_file(listFiles[name]),
+        #                            padx=10)
+        #     checkbtn.grid(row=(f % (len(listFiles) / 2)), column=(f / (len(listFiles) / 2)))
         for f in range(len(listFiles)):
-            checkbtn = Checkbutton(self, text=listFiles[f], command=lambda name=f: self.add_file(listFiles[name]), padx=10)
-            checkbtn.grid(row=(f % (len(listFiles)/ 2)), column=(f / (len(listFiles) / 2)))
-        #for f in range(len(listFiles)):
-        #    checkbtn = Checkbutton(self, text=listFiles[f], command=lambda name=f: self.add_file(listFiles[name]), padx=10)
-        #    checkbtn.grid(row=(f % ((len(listFiles) + len(listFrenchFiles))/ 2)), column=(f / ((len(listFiles) + len(listFrenchFiles)) / 2)))
-        #for f in range(len(listFrenchFiles)):
-        #    checkbtn = Checkbutton(self, text=listFrenchFiles[f], command=lambda name=f: self.add_file(listFrenchFiles[name]), padx=10)
-        #    checkbtn.grid(row=(f % ((len(listFiles) + len(listFrenchFiles))/ 2)), column=(f / ((len(listFiles) + len(listFrenchFiles)) / 2)))
+            checkbtn = Checkbutton(self, text=listFiles[f], command=lambda name=f: self.add_file(listFiles[name]),
+                                   padx=10)
+            checkbtn.grid(row=(f % ((len(listFiles) + len(listFrenchFiles)) / 2)),
+                          column=(f / ((len(listFiles) + len(listFrenchFiles)) / 2)))
+        for f in range(len(listFrenchFiles)):
+            checkbtn = Checkbutton(self, text=listFrenchFiles[f],
+                                   command=lambda name=f: self.add_file(listFrenchFiles[name]), padx=10)
+            checkbtn.grid(row=((f + len(listFiles)) % ((len(listFiles) + len(listFrenchFiles)) / 2)),
+                          column=((f + len(listFiles)) / ((len(listFiles) + len(listFrenchFiles)) / 2)))
 
     def add_file(self, filename):
         """
