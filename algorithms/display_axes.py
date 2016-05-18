c35c473305c42afc9c0649f2ec557696a0881a57
Laura Greige <laura.greige@gmail.com>
2016-05-18 19:57:24 +0200

    Graph/Axes display for BnB

--- algorithms/display_axes.py
+++ algorithms/display_axes.py
@@ -1,11 +1,13 @@
 from os.path import join
 import sys
 from os import getcwd
+
 sys.path.append(getcwd())
 
 from Data.axesPAndroide import *
 from data_gestion.file_gestion import read_file, read_directory
 from find_axis_from_file import find_axis_from_structure
+from algorithms.b_and_b import bnb, find_axes2
 from similarity_matrix import *
 
 
@@ -64,7 +66,9 @@ def axes_to_latex_graph(structure, axis, name=None,
         res += "\\begin{tikzpicture}[x=\\textwidth/" + length_unit + ", y=\\textwidth/" + length_unit + "]\n"
         # Adding the x and y axes
         res += "\\draw[->, >=latex] (0,0) -- (" + length_unit + ",0);\n"
-        res += "\\draw[->, >=latex] (0,0) -- (0," + length_unit + ");\n"
+        res += "\\draw[->, >=latex] (0,0) -- (0, 5.5);\n"
+        #res += "\\draw[->, >=latex] (0,0) -- (" + length_unit + ",0);\n"
+        #res += "\\draw[->, >=latex] (0,0) -- (0," + length_unit + ");\n"
         # Adding the legend below the x axis
         for i in range(len(permutation)):
             res += "\\draw (" + str(i+1) + ",-0.1) node[below] {" + str(permutation[i]) + "};\n"
@@ -86,6 +90,58 @@ def axes_to_latex_graph(structure, axis, name=None,
 
     return res
 
+def axes_to_latex_graph_bnb(f, axis, name=None):
+    """
+    Returns the LaTeX code to display the axis
+    :param structure: structure extracted from a file or directory
+    :param axis: axis corresponding to the file, according to wikipedia
+    :param name: name that will be given to the figures
+    :param dissimilarity_function: function to use to calculate dissimilarity between 2 candidates
+    :param weighted: if True, matrices scores are calculated with the weighted gradient
+    :param unwanted_candidates: list of candidates to exclude from the search
+    :return: string containing the LaTeX code
+    """
+
+    structure = read_file(f)
+    preferences = structure["preferences"]
+    candidates = [i+1 for i in range(len(structure["candidates"]))]
+    bb, best = bnb(len(preferences), preferences, candidates)
+    permutations, card = find_axes2(best[0][0], candidates)
+
+    matches = get_matches(axis)
+    length_unit = str(len(permutations[0]) + 1)
+    res = "\\section{" + str(name) + "}" if name else ""
+    res += "\\begin{center}\n"
+
+    for permutation in permutations:
+        # beginning of the environment
+        res += "\\begin{figure}\n"
+        res += "\\begin{tikzpicture}[x=\\textwidth/" + length_unit + ", y=\\textwidth/" + length_unit + "]\n"
+        # Adding the x and y axes
+        res += "\\draw[->, >=latex] (0,0) -- (" + length_unit + ",0);\n"
+        res += "\\draw[->, >=latex] (0,0) -- (0, 5.5);\n"
+        #res += "\\draw[->, >=latex] (0,0) -- (" + length_unit + ",0);\n"
+        #res += "\\draw[->, >=latex] (0,0) -- (0," + length_unit + ");\n"
+        # Adding the legend below the x axis
+        for i in range(len(permutation)):
+            res += "\\draw (" + str(i+1) + ",-0.1) node[below] {" + str(permutation[i]) + "};\n"
+
+        res += "\\draw plot[ultra thick]coordinates{"
+        temp = ""  # string for the circles marking the dots
+        for i in range(len(permutation)):
+            if permutation[i] in matches:
+                res += "(" + str(i+1) + "," + str(matches[permutation[i]]) + ")"
+                temp += "\\draw[fill=black] (" + str(i+1) + "," + str(matches[permutation[i]]) + ") circle (0.1);\n"
+        res += "};\n"
+        res += temp  # ading the circles
+        res += "\\end{tikzpicture}\n"
+        if name:
+            res += "\\caption{Permutation " + str(permutations.index(permutation)) + " of " + name + "}\n"
+        res += "\\end{figure}\n"
+
+    res += "\\end{center}\n\\clearpage\n"
+
+    return res
 
 def all_files_to_latex(directory, files_list, axes_list, names_list,
                        dissimilarity_function=dissimilarity_over_over, weighted=False, strict=None,
@@ -150,7 +206,6 @@ def launch_irish_glasgow():
     fp = open("Data/TeX/irish_glasgow_and_or_weighted.tex", "w")
     fp.write(s)
     fp.close()
-
 
 def launch_french():
     # on French data
@@ -218,8 +273,14 @@ def launch_french_fusion():
     fp.write(s)
     fp.close()
 
+def launch_french_bnb():
+    s = axes_to_latex_graph_bnb(sys.argv[1], listFrenchAxes[0], name=str(sys.argv[1]))
+    fp = open("Data/TeX/"+str(sys.argv[1]),"w")
+    fp.write(s)
+    fp.close()
 
 if __name__ == '__main__':
     # launch_irish_glasgow()
     # launch_french()
-    launch_french_fusion()
+    # launch_french_fusion()
+    launch_french_bnb()
